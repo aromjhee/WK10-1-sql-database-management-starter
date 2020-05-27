1377c8b676a9a5b7380a451b3a91af56b5c77c6b
Arom Jhee <aromjhee@gmail.com>
2020-05-26 18:58:40 -0700

    create table 'people' 'sections' 'stores'

--- 03-create-aa-times-tables.sql
+++ 03-create-aa-times-tables.sql
@@ -12,7 +12,12 @@ SET ROLE aa_times;
 -- | last_name   | VARCHAR(50)  | NOT NULL    |
 -- | email       | VARCHAR(100) | NOT NULL    |
 
-
+create table people (
+    id serial primary key,
+    first_name varchar(50) not null,
+    last_name varchar(50) not null,
+    email varchar(100) not null
+);
 
 -- The "sections" table
 -- | Column name | Type         | Constraints |
@@ -20,7 +25,10 @@ SET ROLE aa_times;
 -- | id          | SERIAL       | PRIMARY KEY |
 -- | name        | VARCHAR(150) | NOT NULL    |
 
-
+create table sections (
+    id serial primary key,
+    name varchar(150) not null
+);
 
 -- The "stories" table
 -- | Column name | Type    | Constraints                             |
@@ -29,3 +37,12 @@ SET ROLE aa_times;
 -- | author_id   | INTEGER | FOREIGN KEY to people table, NOT NULL   |
 -- | content     | TEXT    | NOT NULL                                |
 -- | section_id  | INTEGER | FOREIGN KEY to sections table, NOT NULL |
+
+create table stories (
+    id serial primary key,
+    author_id integer not null,
+    foreign key (author_id) references people(id),
+    content text not null,
+    section_id integer not null,
+    foreign key (section_id) references sections(id)
+);
